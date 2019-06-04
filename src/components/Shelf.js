112b9fa213c595fa42802376daad5987c70539ca
Austin <austintcanada@hotmail.com>
2019-06-04 15:59:37 -0400

    Added in Card functionality
    Added card form. Added handlers for submit button. Added different card
    states.
    
    Need to add in edit-form functionality where it inserts state into the
    form inputs.

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -5,8 +5,6 @@ export default class Shelf extends Component {
     super();
   }
 
-  
-
   render() {
     return (
       <div className="shelf">
@@ -20,105 +18,158 @@ export default class Shelf extends Component {
   }
 }
 
-
 class Card extends Component {
   constructor() {
     super();
 
+    // add, edit
     this.state = {
       id: '',
       brand: '',
       style: '',
       size: '',
       upcId: '',
+      mode: 'add',
     };
 
   }
 
   handleClick = (option, e) => {
 
-    this.setState({
-      id: this.props.id,
-    }, () => console.log(this.state.id))
+    if (this.state.id === '') {
+      this.setState({
+        id: this.props.id,
+      }, () => console.log(this.state.id))
+    }
 
     if (option === 'add') {
+      this.setState({
+        mode: 'edit',
+      }, () => console.log(this.state.mode));
     }
-    if (option === 'edit') {
-      console.log('Hello from edit!');
+
+    if (option === 'confirm-edit') {
+      this.setState({
+        mode: 'view',
+      });
     }
+
+    if (option === 'cancel-edit') {
+      this.setState({
+        mode: 'view',
+      });
+    }
+
     if (option === 'clear') {
-      console.log('Hello from clear!');
+      this.setState({
+        brand: '',
+        style: '',
+        size: '',
+        upcId: '',
+        mode: 'add',
+      });
     }
   }
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log('target', e.target.brand.value);
+
+    this.setState({
+      brand: e.target.brand.value,
+      style: e.target.style.value,
+      size: e.target.size.value,
+      upcId: e.target.upcId.value,
+      mode: 'view',
+    }, () => console.log('state set by submit'));
+  }
+
+  // edit button might need to be a form confirm button
   render() {
-  const { brand, style, size, upcId } = this.state;
+  const { brand, style, size, upcId, mode } = this.state;
+
     return (
       <div className="card">
-        <ul>
-          <li>{brand}</li>
-          <li>{style}</li>
-          <li>{size}</li>
-          <li>{upcId}</li>
-        </ul>
-        <button 
-          id="button-add"
-          onClick={(e) => this.handleClick('add', e)}
-        ></button>
-        <button 
-          id="button-edit"
-          onClick={(e) => this.handleClick('edit', e)}
-        ></button>
-        <button 
-          id="button-clear"
-          onClick={(e) => this.handleClick('clear', e)}
-        ></button>
+        {
+          mode === 'edit' ? (
+            <>
+              <ShoeForm
+                submitForm={(e) => this.handleSubmit(e)}
+                handleCancel={(e) => this.handleClick('cancel-edit', e)}
+                brand={brand}
+                style={style}
+                size={size}
+                upcId={upcId}
+              ></ShoeForm>
+            </>
+          ) : mode === 'view' ? (
+            <>
+              <StateList
+                brand={brand}
+                style={style}
+                size={size}
+                upcId={upcId}
+              ></StateList>
+              <button 
+                id="button-cancel"
+                onClick={(e) => this.handleClick('add', e)}
+              >edit</button>
+              <button 
+                id="button-cancel"
+                onClick={(e) => this.handleClick('clear', e)}
+              >remove</button>
+            </>
+          ) : (
+            <button 
+              id="button-add"
+              onClick={(e) => this.handleClick('add', e)}
+            >+</button>
+          )
+        }
       </div>
     );
   }
 }
 
-class OptionsMenu extends Component {
-  constructor() {
-    super();
-  }
-
-  render() {
-    return (
-      <div id="options-menu" className="menu">
-      </div>
-    );
-  }
+// make form show parent state
+function ShoeForm(props) {
+  const { submitForm, handleCancel, brand, style, size, upcId } = props;
+  return (
+    <form id="shoe-form" onSubmit={submitForm}>
+      <label>
+        Brand:
+        <input type="text" name="brand" />
+      </label>
+      <label>
+        Style:
+        <input type="text" name="style" />
+      </label>
+      <label>
+        Size:
+        <input type="text" name="size" />
+      </label>
+      <label>
+        UPC ID:
+        <input type="text" name="upcId" />
+      </label>
+      <input type="submit" name="submit" value="accept" />
+      <button
+        onClick={handleCancel}
+      >cancel</button>
+    </form>  
+  );
 }
 
-// EditMenu will be used twice, in edit and add
-// it will just read in from state.
-// If nothing is there already it will "add"
-// if something is there it will "edit"
-class EditMenu extends Component {
-  constructor() {
-    super();
-  }
+function StateList(props) {
+  const { brand, style, size, upcId } = props;
 
-  render() {
-    return (
-      <div id="edit-menu" className="menu">
-      </div>
-    );
-  }
+  return (
+    <ul>
+      <li>{brand}</li>
+      <li>{style}</li>
+      <li>{size}</li>
+      <li>{upcId}</li>
+    </ul>
+  );
 }
-
-class RemoveMenu extends Component {
-  constructor() {
-    super();
-  }
-
-  render() {
-    return (
-      <div id="remove-menu" className="menu">
-      </div>
-    );
-  }
-}
-
-
